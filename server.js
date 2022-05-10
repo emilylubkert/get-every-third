7bea3f0ea406d56a3bc65b391c77cc63e81ca5f5
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-05-10 16:07:26 -0600

    update for testing

--- server.js
+++ server.js
@@ -8,28 +8,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 //return every 3rd letter from string
-app.post('/test', async (req, res) => {
-  const { string_to_cut } = req.body;
-  try {
-    let characters = [];
-    for (i = 2; i < string_to_cut.length; i += 3) {
-        characters.push(string_to_cut.charAt(i));
+const getEveryThird = (req, res) => {
+    const { string_to_cut } = req.body;
+    try {
+      let characters = [];
+      for (i = 2; i < string_to_cut.length; i += 3) {
+          characters.push(string_to_cut.charAt(i));
+      }
+      let newString = characters.join("");
+      if (newString.length === 0) {
+        console.log("Error: Initial string must be at least 3 characters");
+        res.send({"Error": "Initial string must be at least 3 characters"});
+      }
+      else {
+        const returnStrObj = { return_string: newString };
+        console.log('return', returnStrObj);
+        res.status(201).json(returnStrObj);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    let newString = characters.join("");
-    if (newString.length === 0) {
-      console.log("Error: Initial string must be at least 3 characters");
-      res.send({"Error": "Initial string must be at least 3 characters"});
-    }
-    else {
-      const returnStrObj = { return_string: newString };
-      console.log('return', returnStrObj);
-      res.status(201).json(returnStrObj);
-    }
-  } catch (error) {
-    console.log(error);
   }
-});
+app.post('/test', getEveryThird);
 
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
@@ -39,3 +41,5 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server running...`);
 });
+
+export {getEveryThird}
