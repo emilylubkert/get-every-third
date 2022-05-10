ededcc951d67cbf7f3a69616f5d47eff9d65f5bf
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-05-10 16:09:31 -0600

    update tests

--- testRoute.test.js
+++ testRoute.test.js
@@ -1,8 +1,5 @@
 const getEveryThird = require('./testRoute')
 
-//test cases
-//getEveryThird("string")returns every third character for string.length > 2 - pass
-//getEveryThird("string")returns error for string.length <= 2 - pass
 test('Sanity check', () => {
   expect(true).toBe(true);
 });
