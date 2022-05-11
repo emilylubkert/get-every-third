463a607bdee698ace4d0ffe57f9e08892e8c0ca8
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-05-11 13:50:41 -0600

    deploy to heroku

--- server.js
+++ server.js
@@ -32,11 +32,6 @@ const getEveryThird = (req, res) => {
 
 app.post('/test', getEveryThird);
 
-app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server running...`);
 });
