32f9370ed1db6c7676579004f5b33b63e46178ef
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-05-10 20:54:06 -0600

    update file names

---
+++ script.js
@@ -0,0 +1,35 @@
+async function getEveryThird(string_to_cut) {
+  try {
+    let result = await fetch('http://localhost:8080/test', {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ string_to_cut }),
+    });
+    result = await result.json();
+    console.log('response', result);
+    result = JSON.stringify(result);
+    displayResponse(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayResponse = (string) => {
+  const displayDiv = document.getElementById('response');
+  displayDiv.innerHTML = '';
+  const text = document.createElement('h2');
+  text.innerHTML = 'Response: ' + string;
+  displayDiv.appendChild(text);
+}
+
+window.onload = () => {
+  document
+    .getElementById('button')
+    .addEventListener('click', () =>
+      getEveryThird(document.getElementById('input').value)
+    );
+};
+
+module.exports = getEveryThird;
